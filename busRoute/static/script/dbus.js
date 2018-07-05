840a33d32e5ca0fac7d474ea56826891d39094a1
Mary Kate Tyrrell <mary.tyrrell@ucdconnect.ie>
2018-07-05 14:41:55 +0100

    Working with the markerscluster

--- busRoute/static/script/dbus.js
+++ busRoute/static/script/dbus.js
@@ -2,11 +2,12 @@ function myMap() {
     // Function to display the google map on startup
     var mapProp= {
         center:new google.maps.LatLng(53.347515, -6.265377),
-        zoom:12,
+        zoom:8,
     };
         map=new google.maps.Map(document.getElementById("map"),mapProp);
 
 }
+
 // Function used for displaying/hiding the traffic information using a check variable 
 var trafficCheck = 0;
 function toggleTraffic(btn){
