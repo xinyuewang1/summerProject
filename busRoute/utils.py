8dcd8b94e61ebd3aa5c1ac65d8565cb4541a11db
SBean92 <sean.behan@ucdconnect.ie>
2018-07-23 12:28:24 +0100

    commented out currently unused class

--- busRoute/utils.py
+++ busRoute/utils.py
@@ -66,71 +66,71 @@ class Ett39A():
         return routeList
 
 
-class Ann39A:
+# class Ann39A:
 
-    def __init__(self, source, dest, plannedTime, rain, day, distanceTravelled, temp, timeSec, month, date):
-        self.source = source
-        self.dest = dest
-        self.plannedTime = plannedTime
-        self.time = timeSec
-        self.rain = rain
-        self.temp = temp
-        self.distanceTravelled = distanceTravelled
-        self.day = day
-        self.month = month
-        self.date = date
-        routeDict = pickle.load(open('pickles/routeDict.pkl', 'rb'))
-        for key, route in routeDict.items():
-            if route == int(self.source):
-                self.sourceK = key
-            if route == int(self.dest):
-                self.destK = key
-        distanceDict = pickle.load(open('','rb'))
-        for key, distance in distanceDict.items():
-            if key == int(self.source):
-                self.sourceDist = distance
-            if key == int(self.dest):
-                self.destDist = distance
+#     def __init__(self, source, dest, plannedTime, rain, day, distanceTravelled, temp, timeSec, month, date):
+#         self.source = source
+#         self.dest = dest
+#         self.plannedTime = plannedTime
+#         self.time = timeSec
+#         self.rain = rain
+#         self.temp = temp
+#         self.distanceTravelled = distanceTravelled
+#         self.day = day
+#         self.month = month
+#         self.date = date
+#         routeDict = pickle.load(open('pickles/routeDict.pkl', 'rb'))
+#         for key, route in routeDict.items():
+#             if route == int(self.source):
+#                 self.sourceK = key
+#             if route == int(self.dest):
+#                 self.destK = key
+#         distanceDict = pickle.load(open('','rb'))
+#         for key, distance in distanceDict.items():
+#             if key == int(self.source):
+#                 self.sourceDist = distance
+#             if key == int(self.dest):
+#                 self.destDist = distance
     
-    def checkDirection(self):
-        if self.sourceK <= self.destK:
-            return True
-        elif self.sourceK > self.destK:
-            return False
+#     def checkDirection(self):
+#         if self.sourceK <= self.destK:
+#             return True
+#         elif self.sourceK > self.destK:
+#             return False
 
-    def peakTimes(self):
-        peakList = [0]*7
-        for i in range(0, 7):
-            if int(self.time) > 14400+(i*10800) && int(self.time) <= 25200+(i*10800):
-                peakList[i] = 1
-        return peakList
+#     def peakTimes(self):
+#         peakList = [0]*7
+#         for i in range(0, 7):
+#             if int(self.time) > 14400+(i*10800) && int(self.time) <= 25200+(i*10800):
+#                 peakList[i] = 1
+#         return peakList
     
-    def isWeekendOrTerm(self):
-        weekOrTermList = [0]*3
-        weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-        if self.day in weekday:
-            weekOrTermList[0] = 1
-        else:
-            weekOrTermList[1] = 1
-        if self.date >= '2016-01-25' && self.date <= '2016-04-29' 
-            || self.date >= '2016-05-09' && self.date <= '2016-05-21'
-            || self.date >= '2017-01-23' && self.date <= '2016-04-28'
-            || self.date >= '2016-05-08' && self.date <= '2016-05-19':
-            weekOrTermList[3] = 1
-        return weekOrTermList
+#     def isWeekendOrTerm(self):
+#         weekOrTermList = [0]*3
+#         weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+#         if self.day in weekday:
+#             weekOrTermList[0] = 1
+#         else:
+#             weekOrTermList[1] = 1
+#         if self.date >= '2016-01-25' && self.date <= '2016-04-29' 
+#             || self.date >= '2016-05-09' && self.date <= '2016-05-21'
+#             || self.date >= '2017-01-23' && self.date <= '2016-04-28'
+#             || self.date >= '2016-05-08' && self.date <= '2016-05-19':
+#             weekOrTermList[3] = 1
+#         return weekOrTermList
         
 
-    def estimatedTime(self):
-        if self.checkDirection() == True:
-            with open('ann.pkl', 'rb') as f:
-                model = pickle.load(f)
-        else:
-            with open('ann.pkl', 'rb') as f:
-                model = pickle.load(f)
-        if self.sourceK == 1:
-            modelIn = [self.destK, float(self.rain), int(self.day), self.distanceTravelled] + self.peakTimes() + [self.temp] + self.isWeekendOrTerm()
-            return model.predict(modelIn)
-        else:
-            modelInS = [self.sourceK, float(self.rain), int(self.day), self.distanceTravelled] + self.peakTimes() + [self.temp] + self.isWeekendOrTerm()
-            modelInD = [self.destK, float(self.rain), int(self.day), self.distanceTravelled] + self.peakTimes() + [self.temp] + self.isWeekendOrTerm()
-            return model.predict(modelInS) - model.predict(modelInD)
+#     def estimatedTime(self):
+#         if self.checkDirection() == True:
+#             with open('ann.pkl', 'rb') as f:
+#                 model = pickle.load(f)
+#         else:
+#             with open('ann.pkl', 'rb') as f:
+#                 model = pickle.load(f)
+#         if self.sourceK == 1:
+#             modelIn = [self.destK, float(self.rain), int(self.day), self.distanceTravelled] + self.peakTimes() + [self.temp] + self.isWeekendOrTerm()
+#             return model.predict(modelIn)
+#         else:
+#             modelInS = [self.sourceK, float(self.rain), int(self.day), self.distanceTravelled] + self.peakTimes() + [self.temp] + self.isWeekendOrTerm()
+#             modelInD = [self.destK, float(self.rain), int(self.day), self.distanceTravelled] + self.peakTimes() + [self.temp] + self.isWeekendOrTerm()
+#             return model.predict(modelInS) - model.predict(modelInD)
