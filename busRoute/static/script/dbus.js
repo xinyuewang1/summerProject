aaf43cb0d608cabb50e50befdc9d89f3b16395b0
Mary Kate Tyrrell <mary.tyrrell@ucdconnect.ie>
2018-08-08 15:15:15 +0100

    initial page load autocomplete

--- busRoute/static/script/dbus.js
+++ busRoute/static/script/dbus.js
@@ -53,9 +53,9 @@ function myMap() {
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
-   
+
     infoWindow.setContent(browserHasGeolocation ?
-        'Geolocation is Disabled':
+        'Geolocation is Disabled' :
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
     result = "fail";
@@ -94,7 +94,7 @@ function toggleTraffic(btn) {
             center: new google.maps.LatLng(53.347515, -6.265377),
             zoom: 12,
         };
-        
+
         map = new google.maps.Map(document.getElementById("map"), mapProp);
         trafficLayer = new google.maps.TrafficLayer();
         trafficLayer.setMap(map);
@@ -175,9 +175,9 @@ $(document).ready(function () {
 function swapSearch() {
 
     //Checks which option is selected between searching by stops or addresses
-        var c = document.getElementById("stopSearch").style.backgroundColor;
+    var c = document.getElementById("stopSearch").style.backgroundColor;
 
-        if (c != "black") {
+    if (c != "black") {
         //Changes the colour of the tab and placeholder of the search form
         document.getElementById("stopSearch").style.backgroundColor = "black";
 
@@ -206,136 +206,9 @@ function swapSearch() {
         //implementing the autocompletes for the different input fields - source and destination. 
         google.maps.event.addListener(sourceAutocomplete, 'place_changed', function () {
             source = sourceAutocomplete.getPlace();
-            
+
             google.maps.event.addListener(destinationAutocomplete, 'place_changed', function () {
                 destination = destinationAutocomplete.getPlace();
-
-                //Accessing the lattitude and longtitude for the source and destination stop. 
-                sourceLat = source.geometry.location.lat();
-                sourceLong = source.geometry.location.lng();
-                destinationLat = destination.geometry.location.lat();
-                destinationLong = destination.geometry.location.lng();
-                
-
-                //parsing them to create floats. 
-
-                var startLat = parseFloat(sourceLat);
-                var startLng = parseFloat(sourceLong);
-                var finLat = parseFloat(destinationLat);
-                var finLng = parseFloat(destinationLong);
-
-                var directionsService = new google.maps.DirectionsService();
-
-                //creating start and finish google map lattitudes and longtitides.
-                var start = new google.maps.LatLng(startLat, startLng);
-                var end = new google.maps.LatLng(finLat, finLng);
-
-
-                //calculating the route
-                function getStopId() {
-                    var request = {
-                        origin: start,
-                        destination: end,
-                        travelMode: 'TRANSIT',
-                        transitOptions: {
-
-                            modes: ['BUS'],
-                            routingPreference: 'FEWER_TRANSFERS'
-                        },
-                    };
-
-                    directionsService.route(request, function (response, status) {
-                        if (status == 'OK') {
-
-                            //creating a variable to iterte over from the response returned. 
-                            var x = response.routes[0].legs[0].steps;
-
-                            var buses = []
-
-                        
-                            //iterating over the results 
-                            for (var i = 0; i < x.length; i++) {
-
-                                //checking if there is a transit response
-                                if (x[i].transit) {
-
-                                    buses.push(x[i].transit.line.short_name)
-                                    //if there is, access the arrival stop and departure stop lattiitudes and longtitudes. 
-                                    var DLat = x[i].transit.arrival_stop.location.lat();
-                                    var DLong = x[i].transit.arrival_stop.location.lng();
-                                    var SLat = x[i].transit.departure_stop.location.lat();
-                                    var Slong = x[i].transit.departure_stop.location.lng();
-
-                                    //i had issues where what was being returned was rounded off differently than the stops we have in our data so I rounded them
-                                    var destLatitude = parseFloat(DLat.toFixed(4));
-                                    var destLongtitude = parseFloat(DLong.toFixed(4));
-                                    var sourceLatitude = parseFloat(SLat.toFixed(3));
-                                    var sourceLongtitude = parseFloat(Slong.toFixed(3))
-
-                                    //access our bus data because we need to get the stop number for the model. 
-
-                                   
-                                    $.ajax({
-                                        type: "GET",
-                                        url: "dublinBusInfo",
-                                        dataType: "json",
-
-                                        success: function (data) {
-
-
-                                            //Iterate over the data
-                                            for (var i = 0; i < data.length; i++) {
-
-                                                //creating variables from our data to compare to the lattitudes and longtitudes from the returned arrival and destination stops from google. 
-                                                var comparisonLat = parseFloat(data[i].lat).toFixed(4)
-                                                var comparisonLong = parseFloat(data[i].lng).toFixed(4)
-                                                var comparisonLat1 = parseFloat(data[i].lat).toFixed(3)
-                                                var comparisonLong1 = parseFloat(data[i].lng).toFixed(3)
-
-
-                                                //Check if they match, return the num and name
-                                                if (destLatitude == comparisonLat && destLongtitude == comparisonLong) {
-
-                                                    console.log(data[i].num);
-                                                    console.log(data[i].name);
-
-
-                                                }
-
-                                                //check if they match then return the num and name
-                                                if (sourceLatitude == comparisonLat1 && sourceLongtitude == comparisonLong1) {
-
-                                                    console.log(data[i].num);
-                                                    console.log(data[i].name);
-
-                                                }
-
-                                            }
-
-                                            //view the routes that can be taken.
-                                            console.log(buses);
-
-                                        }
-                                    });
-
-
-
-
-
-                                }
-
-
-                            }
-
-                        }
-
-                    })
-
-
-
-                }
-
-                 getStopId();
 
             })
         })
@@ -550,6 +423,38 @@ function findRoute() {
         document.getElementById("routeSearchOptions").style.display = "none";
     }
 }
+
+
+
+//autocomplete for stop search on page load
+
+$("#id_souce, #id_destination").ready(function (){
+$.ajax({
+    type: "GET",
+    url: "RouteInfo",
+    dataType: "json",
+
+    success: function (data) {
+        createArray(data);
+    }
+});
+
+})
+
+
+function createArray(Data) {
+
+    results = [];
+    for (var i = 0; i < Data.length; i++) {
+        results.push(Data[i].num);
+    }
+    $("#id_destination, #id_source").autocomplete({
+        source: results,
+        minLength: 2,
+    });
+
+}
+
 
 
 // References:
